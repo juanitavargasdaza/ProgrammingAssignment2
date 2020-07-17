d9d7f97c04898789c65d0d5fe235647865759e3d
Juanita Vargas Daza <68071108+juanitavargasdaza@users.noreply.github.com>
2020-07-17 12:17:29 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+Esta funcion permite hacer calculos muy rapidos, que procedes de una matriz de vectores muy largos 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+  }
+  get <- function()(x)
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
+Con esta funcion cuando los datos de una matriz son muy largos, la operacion se puede ejecutar delsde el cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         inv <- x$getInverse()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
